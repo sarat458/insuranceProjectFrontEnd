a45454fea3598b7b583fa0efc99d4ff40b5b15a7
sarat458 <srthkmr491@gmail.com>
2021-11-03 12:29:42 -0700

    Update user.js
    added Navbar component

--- src/User.js
+++ src/User.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-
+import Navbar from './Navbar'
 export default class User extends Component {
     render() {
         return (
-            <div>
-                <h1>User Page</h1>
-            </div>
+            <Navbar />
         )
     }
 }
