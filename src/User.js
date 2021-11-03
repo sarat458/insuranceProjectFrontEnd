5f79daf7f3a800304bdb91e8e2cf8fe55843bca2
sarat458 <srthkmr491@gmail.com>
2021-11-03 12:49:17 -0700

    UserComponent created

--- src/User.js
+++ src/User.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
+import UserComponent from './UserComponent'
 export default class User extends Component {
     render() {
         return (
-            <Navbar />
+            <div>
+                <Navbar />
+                <UserComponent />
+                <Footer />
+            </div>
         )
     }
 }
---
+++ src/UserComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UserComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{ textAlign: "center" }}>Hello User</h2>
+            </div>
+        )
+    }
+}
