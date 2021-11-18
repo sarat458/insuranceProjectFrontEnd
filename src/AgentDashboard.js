9573a6d3e968b38ef897c3506b34188ff5bb11d8
sarat458 <srthkmr491@gmail.com>
2021-11-18 00:33:43 -0800

    save

--- src/AgentDashboard.js
+++ src/AgentDashboard.js
@@ -159,7 +159,7 @@ export default class AgentDashboard extends Component {
         return (
             <React.Fragment>
                 <div className="mt-5">
-                    <div className="row" style={{ height: "300px" }}>
+                    <div className="row" style={{ maxHeight: "300px" }}>
                         <div className="col mr-5 ml-3">
                             <h4>Prospective Customers</h4>
                             <table className="table table-bordered border-dark ">
@@ -177,7 +177,7 @@ export default class AgentDashboard extends Component {
                         </div>
                         <div className="col mr-5">
                             <h4>Enrolled Customers</h4>
-                            <table className="table table-bordered border-dark">
+                            <table className="table">
                                 <thead>
                                     <tr>
                                         <th scope="col">#</th>
@@ -195,7 +195,7 @@ export default class AgentDashboard extends Component {
                     <br /><br />
                     <div className="row">
                         <div className="col">
-                            <div className="card p-4" style={{ width: '18rem', height: '184.4px' }}>
+                            <div className="card p-4" style={{ width: '18rem', maxHeight: '184.4px' }}>
                                 <div className="card-body">
                                     <h3 className="card-title text-center">Monthly Stats</h3>
                                     <h6 className="card-subtitle mb-2 text-muted mx-5 text-center">{this.state.monthlyStats.totalNumberOfPolicies} policies sold</h6>
@@ -205,7 +205,7 @@ export default class AgentDashboard extends Component {
                         </div>
 
                         <div className="col">
-                            <div className="card p-4" style={{ width: '18rem', height: '184.4px' }}>
+                            <div className="card p-4" style={{ width: '18rem', maxHeight: '184.4px' }}>
                                 <div className="card-body">
                                     <h3 className="card-title text-center">Weekly Stats</h3>
                                     <h6 className="card-subtitle mb-2 text-muted mx-5 text-center">{this.state.weeklyStats.totalNumberOfPolicies} policies sold</h6>
--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Carousel from 'react-bootstrap/Carousel'
 import React, { Component } from 'react'
 import Navbar from './Navbar'
-
+import auto from '../src/assets/img/auto.jpg'
+import health from '../src/assets/img/health.jpeg'
+import property from '../src/assets/img/property.jpg'
 
 
 export default class Home extends Component {
@@ -13,7 +15,46 @@ export default class Home extends Component {
                 <Navbar />
 
                 <div className="row">
-                    <div className="col text-center"><h3>Carousal</h3></div>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                height={500}
+                                src={auto}
+                                alt="Auto Insurance"
+                            />
+                            <Carousel.Caption>
+                                <h3 className="text-light">Auto Insurance</h3>
+                                <p>Heritage, State Farm and Liberty</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={health}
+                                height={500}
+                                alt="Health Insurance"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Property Insurance</h3>
+                                <p>Heritage, State Farm and Liberty</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={property}
+                                height={500}
+                                alt="Property Insurance"
+                            />
+
+                            <Carousel.Caption>
+                                <h3>Health Insurance</h3>
+                                <p>Heritage, State Farm and Liberty</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
                 </div>
                 <hr />
                 <div className="row">
